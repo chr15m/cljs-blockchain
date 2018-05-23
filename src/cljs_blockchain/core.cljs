4461be4c737630af817498c21cbd9f9abd0b00fd
Chris McCormick <chris@mccormick.cx>
2018-05-23 17:11:40 +0800

    Main event loop architecture up & running.

--- src/cljs_blockchain/core.cljs
+++ src/cljs_blockchain/core.cljs
@@ -1,15 +1,50 @@
 (ns cljs-blockchain.core
-    (:require
-      ["bencode-js/lib/index" :as bencode]
-      ["scrypt-js/scrypt" :as scrypt]
-      [cljsjs.webtorrent :as wt]
-      [cljsjs.nacl-fast :as nacl]
-      [reagent.core :as r]))
+  (:require
+    [cljs.core.async :refer [chan <! timeout put!]]
+    ["bencode-js/lib/index" :as bencode]
+    ["scrypt-js/scrypt" :as scrypt]
+    [cljsjs.webtorrent :as wt]
+    [cljsjs.nacl-fast :as nacl]
+    [reagent.core :as r])
+  (:require-macros
+    [cljs.core.async.macros :refer [go go-loop]]))
 
-(js/console.log (wt.))
-(js/console.log (nacl.randomBytes 32))
-(js/console.log (bencode/encode #js {:a 12}))
-(scrypt (js/Uint8Array. [1 2 3 4]) (js/Uint8Array. ["a" "b" "c"]) 1024 8 1 32 js/console.log)
+(defonce state (r/atom {}))
+
+;*** data manipulation ***;
+
+(defn to-hex [b]
+  (.join (.map (js/Array.from (.slice b)) #(.slice (str "0" (.toString (bit-and % 0xff) 16)) -2)) ""))
+
+(defn from-hex [b]
+  (js/Uint8Array. (map #(js/parseInt (apply str %) 16) (partition 2 b))))
+
+(defn pk [state]
+  (-> state
+      :keypair
+      (or #js {})
+      (.. -publicKey)
+      (or #js [])
+      (to-hex)))
+
+;*** main event loop ***;
+
+(defn main-loop [old-state]
+  (go
+    (js/console.log "main-loop old-state:" (clj->js old-state))
+    (let [new-state old-state
+          ; ensure incoming channel
+          new-state (update-in new-state [:incoming] #(or % (chan)))
+          ; ensure wallet keys
+          new-state (update-in new-state [:keypair] #(or % (nacl.sign.keyPair)))
+          ; ensure webtorrent instance
+          ;new-state (update-in new-state [:net] #(or % (wt.)))
+          ; ensure blockchain structure
+          ; ensure webtorrent connection
+          ; wait for events
+          event (<! (new-state :incoming))]
+      (js/console.log "event:" (clj->js event))
+      new-state)))
 
 ;; -------------------------
 ;; Views
@@ -21,7 +56,9 @@
 ;; Initialize app
 
 (defn mount-root []
-  (r/render [home-page] (.getElementById js/document "app")))
+  ;(put! (@state :incoming) :init)
+  (r/render [home-page state] (.getElementById js/document "app")))
 
 (defn init! []
+  (go-loop [] (reset! state (<! (main-loop @state))) (recur))
   (mount-root))
