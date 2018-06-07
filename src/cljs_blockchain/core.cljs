fbe2c6a7521fb77a00ea7ed5dabc1853360c03cc
Chris McCormick <chris@mccormick.cx>
2018-06-07 21:56:02 +0800

    TODO list update.

--- src/cljs_blockchain/core.cljs
+++ src/cljs_blockchain/core.cljs
@@ -11,11 +11,12 @@
     [cljs.core.async.macros :refer [go go-loop]]))
 
 ; TODO:
-; * add index to blocks
+; * remove epoch code
+; * validate blockchain when a block is added
+; * compute POW at mining stage
 ; * block headers: version, previous-block-hash, transaction-merkle-root-hash, timestamp, difficulty, nonce
 ;   https://en.bitcoin.it/wiki/Block_hashing_algorithm
 ; * sign transactions
-; * include nonce in coinbase transaction (to update merkle root)
 
 (defonce state (r/atom {:incoming (chan)}))
 
